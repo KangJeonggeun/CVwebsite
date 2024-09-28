9db49d184dc1882670faaeda38baa26aa2794788
unknown <jeonggeun.kang@gmail.com>
2024-09-28 21:59:08 +0200

    email form alignment

--- components/home/emailContact.js
+++ components/home/emailContact.js
@@ -10,6 +10,7 @@ import {
   Textarea,
   useToast,
   Flex,
+  Box,
 } from "@chakra-ui/react";
 import { createContext, useState } from "react";
 import { sendContactForm } from "../../lib/api";
@@ -77,147 +78,166 @@ export default function Contact() {
   };
 
   return (
-    <Container maxW="900px" mt={12} textAlign="center">
-      <Heading fontSize="30px" padding="10px">
-        Contact
-      </Heading>
-      {error && (
-        <Text color="red.300" my={4} fontSize="xl">
-          {error}
-        </Text>
-      )}
+    <Flex justify="center" align="center" minHeight="100vh" width="100%">
+      <Container mt={12} textAlign="center">
+        {/* maxW="900px" */}
+        <Heading fontSize="30px" padding="10px">
+          Contact
+        </Heading>
+        {error && (
+          <Text color="red.300" my={4} fontSize="xl">
+            {error}
+          </Text>
+        )}
 
-      <FormControl
-        isRequired
-        isInvalid={touched.name && !values.name}
-        mb={5}
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-      >
-        <FormLabel textAlign="center" fontSize="25px">
-          Name
-        </FormLabel>
-        <Input
-          type="text"
-          name="name"
-          errorBorderColor="red.300"
-          value={values.name}
-          onChange={handleChange}
-          onBlur={onBlur}
-          width="75%"
-          h="50px"
-        />
-        <FormErrorMessage
-          // textAlign="center"
-          // width="75%"
+        <FormControl
+          isRequired
+          isInvalid={touched.name && !values.name}
+          mb={5}
           display="flex"
-          justifyContent="center"
-          // color="red.300" Need to check global css
+          flexDirection="column"
+          alignItems="center"
         >
-          Name is Required
-        </FormErrorMessage>
-      </FormControl>
+          <FormLabel textAlign="center" fontSize="25px">
+            Name
+          </FormLabel>
+          <Input
+            // width={["100%", "75%", "60%"]}
+            type="text"
+            name="name"
+            errorBorderColor="red.300"
+            value={values.name}
+            onChange={handleChange}
+            onBlur={onBlur}
+            width="350%"
+            h="50px"
+          />
+          <FormErrorMessage
+            // textAlign="center"
+            // width="75%"
+            display="flex"
+            justifyContent="center"
+            // color="red.300" Need to check global css
+          >
+            Name is Required
+          </FormErrorMessage>
+        </FormControl>
 
-      <FormControl isRequired isInvalid={touched.email && !values.email} mb={5}>
-        <FormLabel textAlign="center" fontSize="25px">
-          Email
-        </FormLabel>
-        <Input
-          type="email"
-          name="email"
-          errorBorderColor="red.300"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={onBlur}
-          width="75%"
-          h="50px"
-        />
-        <FormErrorMessage display="flex" justifyContent="center">
-          Email is Required
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl
-        mb={5}
-        isRequired
-        isInvalid={touched.subject && !values.subject}
-      >
-        <FormLabel textAlign="center" fontSize="25px">
-          Subject
-        </FormLabel>
-        <Input
-          type="text"
-          name="subject"
-          errorBorderColor="red.300"
-          value={values.subject}
-          onChange={handleChange}
-          onBlur={onBlur}
-          width="75%"
-          h="50px"
-        />
-        <FormErrorMessage display="flex" justifyContent="center">
-          Subject is Required
-        </FormErrorMessage>
-      </FormControl>
-
-      <FormControl
-        isRequired
-        isInvalid={touched.message && !values.message}
-        mb={5}
-      >
-        <FormLabel textAlign="center" fontSize="25px">
-          Message
-        </FormLabel>
-        <Textarea
-          type="text"
-          name="message"
-          rows={4}
-          errorBorderColor="red.300"
-          value={values.message}
-          onChange={handleChange}
-          onBlur={onBlur}
-          width="75%"
-          h="100px"
-        />
-        <FormErrorMessage display="flex" justifyContent="center">
-          Message is Required
-        </FormErrorMessage>
-      </FormControl>
-      <Flex justifyContent="center" mb={5}>
-        {" "}
-        {/* Centering Flex Container for Button */}
-        <Button
-          variant="outline"
-          colorScheme="blue"
-          isLoading={isLoading}
-          disabled={
-            !values.name || !values.email || !values.subject || !values.message
-          }
-          onClick={onSubmit}
-          sx={{
-            background: `linear-gradient(90deg, #2193b0 0%, #6dd5ed 50%, #2193b0 100%), linear-gradient(0deg, #ffffff, #ffffff)`,
-            backgroundSize: "200% 100%", // Size of the gradient background
-            transition: "background-position 0.3s ease", // Smooth transition
-          }}
-          _hover={{
-            backgroundPosition: "100% 0", // Change background position on hover
-          }}
-          paddingY="0.5rem" // Adjust vertical padding
-          paddingX="1.75rem" // Adjust horizontal padding
-          borderRadius="full" // Adjust border radius if needed
-          fontWeight="medium" // Font weight
-          fontSize="30px" // Font size
-          display="flex" // Display as flex
-          // alignItems="center"
+        <FormControl
+          isRequired
+          isInvalid={touched.email && !values.email}
+          mb={5}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
         >
-          Submit
-        </Button>
-      </Flex>
+          <FormLabel textAlign="center" fontSize="25px">
+            Email
+          </FormLabel>
+          <Input
+            type="email"
+            name="email"
+            errorBorderColor="red.300"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={onBlur}
+            width="350%"
+            h="50px"
+          />
+          <FormErrorMessage display="flex" justifyContent="center">
+            Email is Required
+          </FormErrorMessage>
+        </FormControl>
 
-      {/* <Button
+        <FormControl
+          mb={5}
+          isRequired
+          isInvalid={touched.subject && !values.subject}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <FormLabel textAlign="center" fontSize="25px">
+            Subject
+          </FormLabel>
+          <Input
+            type="text"
+            name="subject"
+            errorBorderColor="red.300"
+            value={values.subject}
+            onChange={handleChange}
+            onBlur={onBlur}
+            width="350%"
+            h="50px"
+          />
+          <FormErrorMessage display="flex" justifyContent="center">
+            Subject is Required
+          </FormErrorMessage>
+        </FormControl>
+
+        <FormControl
+          isRequired
+          isInvalid={touched.message && !values.message}
+          mb={5}
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <FormLabel textAlign="center" fontSize="25px">
+            Message
+          </FormLabel>
+          <Textarea
+            type="text"
+            name="message"
+            rows={4}
+            errorBorderColor="red.300"
+            value={values.message}
+            onChange={handleChange}
+            onBlur={onBlur}
+            width="350%"
+            h="100px"
+          />
+          <FormErrorMessage display="flex" justifyContent="center">
+            Message is Required
+          </FormErrorMessage>
+        </FormControl>
+        <Flex justifyContent="center" mb={5}>
+          {" "}
+          {/* Centering Flex Container for Button */}
+          <Button
+            variant="outline"
+            colorScheme="blue"
+            isLoading={isLoading}
+            disabled={
+              !values.name ||
+              !values.email ||
+              !values.subject ||
+              !values.message
+            }
+            onClick={onSubmit}
+            sx={{
+              background: `linear-gradient(90deg, #2193b0 0%, #6dd5ed 50%, #2193b0 100%), linear-gradient(0deg, #ffffff, #ffffff)`,
+              backgroundSize: "200% 100%", // Size of the gradient background
+              transition: "background-position 0.3s ease", // Smooth transition
+            }}
+            _hover={{
+              backgroundPosition: "100% 0", // Change background position on hover
+            }}
+            paddingY="0.5rem" // Adjust vertical padding
+            paddingX="1.75rem" // Adjust horizontal padding
+            borderRadius="full" // Adjust border radius if needed
+            fontWeight="medium" // Font weight
+            fontSize="30px" // Font size
+            display="flex" // Display as flex
+            // alignItems="center"
+          >
+            Submit
+          </Button>
+        </Flex>
+
+        {/* <Button
         classes="mr-3"
-     
+
         type={ButtonTypes.PRIMARY}
         name="SUBMIT"
         otherProps={{
@@ -226,6 +246,7 @@ export default function Contact() {
           onClick: { onSubmit },
         }}
       ></Button> */}
-    </Container>
+      </Container>
+    </Flex>
   );
 }
